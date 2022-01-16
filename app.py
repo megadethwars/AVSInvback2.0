0f9df879d5f3be7593bb7c0b141e82d784b559fd
DESKTOP-FGFDBVD\leone <leonellopezvazquez2@gmail.com>
2022-01-16 00:59:23 -0600

    library

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 import os
 #from dotenv import load_dotenv, find_dotenv
 from flask import Flask, current_app, render_template
-from .src.appinit import create_app
+from src.appinit import create_app
 #load_dotenv(find_dotenv())
 
 env_name = 'local'
@@ -12,7 +12,7 @@ env_name = 'local'
 app = Flask(__name__)
 
 @app.route("/")
-def index():    
+def index():
     return "Hola"
 
 if __name__ == '__main__':
