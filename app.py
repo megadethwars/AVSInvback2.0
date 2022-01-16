7e7820f9924ae603ffb6b106ba79f2851472808f
DESKTOP-FGFDBVD\leone <leonellopezvazquez2@gmail.com>
2022-01-16 00:45:18 -0600

    modify

--- app.py
+++ app.py
@@ -2,15 +2,20 @@
 import os
 #from dotenv import load_dotenv, find_dotenv
 from flask import Flask, current_app, render_template
-from .src.appinit import create_app
+#from .src.appinit import create_app
 #load_dotenv(find_dotenv())
 
 env_name = 'local'
-app = create_app(env_name)
+#app = create_app(env_name)
 
 
+app = Flask(__name__)
+
+@app.route("/")
+def index():    
+    return "Hola"
 
 if __name__ == '__main__':
 
   # run app
-  app.run(host='0.0.0.0', port=5000)
+  app.run(debug=True)
